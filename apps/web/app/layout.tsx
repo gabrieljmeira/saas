3701c841512cf9dc3ba25c6a3be6dbdf7a04ed0e
gabrieljmeira <gabrieljustin1234@gmail.com>
2026-08-30 22:11:46 -0300

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   apps/web/app/layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,11 +22,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
       lang="pt-BR"
       className={`dark ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
       <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
